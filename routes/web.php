d5f95d373a94759dd6615cfe6a2dffdf97afcc10
JaimeMottaSena <13alejandromottamesa@gmail.com>
2021-04-28 11:32:03 -0500

    arreglos multidimencionales

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,41 @@ Route::get('/', function () {
 Route::get('variables' , function(){
     echo "hola";
 });
+
+Route::get("paises",function(){
+    $paises=["Colombia"=>[
+            "capital"=>"Bogota",
+            "moneda"=>"Peso",
+            "poblacion"=> 51
+    ] ,
+    "Peru"=>[
+        "capital"=>"Lima",
+        "moneda"=>"Sol",
+        "poblacion"=> 32.8
+    ],
+    "Paraguay"=>[
+        "capital"=>"Asuncion",
+        "moneda"=>"Guarani",
+        "poblacion"=> 7.3
+
+    ]
+];
+//el tipo de datos hay 2 lenguajes , fuertemente equipado:se define el tipo de dato y no cambia
+// php, js son lenguajes devilmente equipados no es necesario definir el dato
+//en php no existe el problema de definir las variables como entero etc
+//concaenar pgar 2 cadenas de caracteres en php es con .
+// solo para recorrer arreglos en php foreach
+    foreach($paises as  $nombre => $pais){
+        echo "<pre>";
+        echo"<h1>$nombre</<h1>". "<br/>";
+        print_r($pais["capital"]. "<br/>");
+        print_r($pais["poblacion"]. "<br/>");
+        print_r($pais["moneda"]. "<br/>");
+        echo"</pre>";
+        echo "<hr />";
+
+    }
+
+
+
+});
